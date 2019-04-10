c4b8a3577810bc870647c82f615fbf05e7a29eee
ddrampi <eriks.ejubs@icloud.com>
2019-04-10 19:51:02 +0300

    added roles controllers

---
+++ src/main/java/io/codelex/securityapp/authentication/UnitAuthenticationController.java
@@ -0,0 +1,37 @@
+package io.codelex.securityapp.authentication;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api")
+class UnitAuthenticationController {
+    private final AuthService authService;
+
+    UnitAuthenticationController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/sign-in")
+    public void signIn(@RequestParam("email") String email ,
+                       @RequestParam("password") String password) {
+        authService.authorise(email, password, "ROLE_UNIT");
+    }
+
+    @PostMapping("/register")
+    public void register(@RequestParam("email") String email ,
+                         @RequestParam("password") String password) {
+        authService.authorise(email, password, "ROLE_UNIT");
+    }
+
+    @PostMapping("/sign-out")
+    public void signOut() {
+        authService.clearAuthentication();
+    }
+
+    @GetMapping("/account")
+    public String account(Principal principal) {
+        return principal.getName();
+    }
+}
