52790ea56a101843017f659be771e4f6bdb6417c
denijs <denijs.balodis@inbox.lv>
2019-04-11 16:01:41 +0300

    updating progress

--- src/test/java/io/codelex/securityapp/route/RouteGatewayTest.java
+++ src/test/java/io/codelex/securityapp/route/RouteGatewayTest.java
@@ -1,46 +1,68 @@
 package io.codelex.securityapp.route;
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.codelex.securityapp.repository.models.Client;
 import io.codelex.securityapp.repository.models.Incident;
 import io.codelex.securityapp.repository.models.Unit;
+import org.junit.Rule;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
 import java.math.BigDecimal;
+import java.nio.file.Files;
 
-@RunWith(SpringRunner.class)
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
 class RouteGatewayTest {
+    @Rule
+    static WireMockRule wireMock = new WireMockRule();
+    private RouteGateway routeGateway;
 
-    RouteGateway routeGateway;
+    @BeforeAll
+    static void setUpOnce() {
+        wireMock.start();
+    }
 
     @BeforeEach
     void setUp() {
         GoogleMapsProps props = new GoogleMapsProps();
-        props.setApiUrl("https://maps.googleapis.com/maps/api/distancematrix/json?units=metric");
-        props.setApiKey("AIzaSyDcsLDxrAteVZEOc4h8zBJwVzs13Cb79OU");
-
+        props.setApiUrl("http://localhost:" + wireMock.port());
+        props.setApiKey("123");
         routeGateway = new RouteGateway(props);
     }
 
     @Test
-    void should_get_distance_from_unit_to_incident() {
+    void should_fetch_distance() throws Exception {
         //given
         Unit unit = new Unit(
-                new BigDecimal(56.941887),
-                new BigDecimal(24.095740),
+                new BigDecimal(24.941887),
+                new BigDecimal(56.095740),
                 true
         );
         Incident incident = new Incident(
                 new Client("name", "surname"),
-                new BigDecimal(56.254896),
-                new BigDecimal(24.113705)
+                new BigDecimal(24.113705),
+                new BigDecimal(56.254896)
         );
-        //then
-        Long distanceTo = routeGateway.calculateRoute(unit, incident);
-        Assertions.assertEquals(84174, distanceTo);
-    }
+        
+        File file = ResourceUtils.getFile(this.getClass().getResource("/stubs/successful-response.json"));
+        Assertions.assertTrue(file.exists());
+        
+        byte [] json = Files.readAllBytes(file.toPath());
 
+        wireMock.stubFor(get(urlPathEqualTo("/maps/api/distancematrix/json?units=metric"))
+                .withQueryParam("Key", equalTo("123"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json; charset=utf-8")
+                        .withStatus(200)
+                        .withBody(json)));
+        //when
+        Long distance = routeGateway.calculateRoute(unit, incident);
+        //then
+        Assertions.assertEquals(128773, distance);
+    }
 }
