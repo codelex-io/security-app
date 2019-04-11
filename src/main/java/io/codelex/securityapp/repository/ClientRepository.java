12f4a650d88016c500f0a651404256c26af1f167
Martins Akermanis <m.akermanis@gmail.com>
2019-04-11 21:21:14 +0300

    can register client

--- src/main/java/io/codelex/securityapp/repository/ClientRepository.java
+++ src/main/java/io/codelex/securityapp/repository/ClientRepository.java
@@ -4,4 +4,5 @@ import io.codelex.securityapp.repository.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
 }
